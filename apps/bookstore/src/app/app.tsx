a9c89de655042035edabebca168dd6b29de63cae
Mumtahin Farabi <mfarabi619@gmail.com>
2024-11-09 22:45:27 -0500

    feat(ui/bookstore,libs/books): generate and render lib

--- apps/bookstore/src/app/app.tsx
+++ apps/bookstore/src/app/app.tsx
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+import { Route, Routes, Link } from 'react-router-dom';
+
+import { Feature } from '@learning-nx/feature';
+
 /* import NxWelcome from './nx-welcome'; */
 
 const StyledApp = styled.div`
@@ -11,8 +15,49 @@ export function App() {
     <StyledApp>
       {/* <NxWelcome title="bookstore" /> */}
       <header>
-      <h1>Bookstore</h1>
+        <h1>Bookstore</h1>
       </header>
+
+      {/* START: routes */}
+      {/* These routes and navigation have been generated for you */}
+      {/* Feel free to move and update them to fit your needs */}
+      <br />
+      <hr />
+      <br />
+      <div role="navigation">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/feature">Feature</Link>
+          </li>
+          <li>
+            <Link to="/page-2">Page 2</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              This is the generated root route.{' '}
+              <Link to="/page-2">Click here for page 2.</Link>
+            </div>
+          }
+        />
+        <Route path="/feature" element={<Feature />} />
+        <Route
+          path="/page-2"
+          element={
+            <div>
+              <Link to="/">Click here to go back to root page.</Link>
+            </div>
+          }
+        />
+      </Routes>
+      {/* END: routes */}
     </StyledApp>
   );
 }
