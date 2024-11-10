dfad3a7bad46412f4555abf94abb2f02aaea930f
Mumtahin Farabi <mfarabi619@gmail.com>
2024-11-10 18:38:20 -0500

    feat(ui/bookstore): render data from data-access library

--- libs/books/feature/src/lib/books-feature.tsx
+++ libs/books/feature/src/lib/books-feature.tsx
@@ -1,15 +1,23 @@
-import styled from 'styled-components';
+import { useEffect, useState } from 'react'
+import {getBooks } from '@learning-nx/books/data-access'
+import {Books} from '@learning-nx/books/ui'
 
-const StyledFeature = styled.div`
-  color: pink;
-`;
+export const BooksFeature = () => {
+  const [books, setBooks] = useState<any[]>([]);
 
-export function BooksFeature() {
-  return (
-    <StyledFeature>
-      <h1>Welcome to BooksFeature!</h1>
-    </StyledFeature>
-  );
+  useEffect(() => {
+    getBooks().then(setBooks);
+},[
+  // This effect runs only once on first component render
+  /* so we declare it as having no dependent state. */
+])
+
+return  (
+  <>
+    <h2>Books</h2>
+   <Books books={books}/>
+  </>
+)
 }
 
 export default BooksFeature;
---
+++ libs/books/ui/src/lib/books-ui.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const StyledBooksUi = styled.div`
+  color: pink;
+`;
+
+export function BooksUi() {
+  return (
+    <StyledBooksUi>
+      <h1>Welcome to BooksUi!</h1>
+    </StyledBooksUi>
+  );
+}
+
+export default BooksUi;
