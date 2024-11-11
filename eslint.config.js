6a3833614807668b18c3f3db109d48251d5af9e3
Mumtahin Farabi <mfarabi619@gmail.com>
2024-11-10 19:05:07 -0500

    chore(config/eslint): create dependency rules

--- eslint.config.js
+++ eslint.config.js
@@ -13,15 +13,28 @@ module.exports = [
       '@nx/enforce-module-boundaries': [
         'error',
         {
+          "allowCircularSelfDependency": true,
           enforceBuildableLibDependency: true,
           allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
           depConstraints: [
             {
-              sourceTag: '*',
-              onlyDependOnLibsWithTags: ['*'],
+              sourceTag: 'type:app',
+              onlyDependOnLibsWithTags: ['type:feature', "type:ui", "type:util"],
+            },
+            {
+              sourceTag: 'type:feature',
+              onlyDependOnLibsWithTags: ['type:feature', "type:ui", "type:util"],
+            },
+            {
+              sourceTag: 'type:ui',
+              onlyDependOnLibsWithTags: ["type:ui", "type:util"],
+            },
+            {
+              sourceTag: 'type:util',
+              onlyDependOnLibsWithTags: ["type:util"],
             },
           ],
-        },
+        }
       ],
     },
   },
