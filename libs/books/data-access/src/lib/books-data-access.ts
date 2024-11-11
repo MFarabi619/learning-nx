5f09020e00a1e10e9a7d60261b27d4600b19f4ef
Mumtahin Farabi <mfarabi619@gmail.com>
2024-11-10 20:26:46 -0500

    refactor(libs/books/data-access): replace any with book type from shared models

--- libs/books/data-access/src/lib/books-data-access.ts
+++ libs/books/data-access/src/lib/books-data-access.ts
@@ -1,4 +1,6 @@
-export async function getBooks() {
+import {IBook} from '@learning-nx/shared-models';
+
+export async function getBooks(): Promise<IBook[]> {
   const data = await fetch('/api/books', {
     headers: {
       'Content-Type': 'application/json'
