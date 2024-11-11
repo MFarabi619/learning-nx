997c0fe83c6938416bf106428baca80912d11af3
Mumtahin Farabi <mfarabi619@gmail.com>
2024-11-10 21:52:51 -0500

    feat(ui/bookstore): add route

--- apps/bookstore/src/app/app.tsx
+++ apps/bookstore/src/app/app.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes, Navigate, Link } from 'react-router-dom';
 
 import { BooksFeature } from '@learning-nx/books/feature';
+import { CartFeature } from '@learning-nx/cart/feature';
 
 // importing the UI library into our App
 import {
@@ -8,28 +9,32 @@ import {
   Header,
   Main,
   NavigationItem,
-  NavigationList
-} from '@learning-nx/ui'
+  NavigationList,
+} from '@learning-nx/ui';
 
 
 export function App() {
   return (
     <>
-    <GlobalStyles />
+      <GlobalStyles />
       <Header>
         <h1>Bookstore</h1>
         <NavigationList>
- <NavigationItem>
-   <Link to="/books">Books</Link>
- </NavigationItem>
+          <NavigationItem>
+            <Link to="/books">Books</Link>
+          </NavigationItem>
+          <NavigationItem>
+            <Link to="/cart">Cart</Link>
+          </NavigationItem>
         </NavigationList>
       </Header>
-<Main>
-      <Routes>
-        <Route path="/books" element={<BooksFeature />} />
-        <Route path="/" element={<Navigate to="/books" replace />} />
-      </Routes>
-    </Main>
+      <Main>
+        <Routes>
+          <Route path="/books" element={<BooksFeature />} />
+          <Route path="/cart" element={<CartFeature />} />
+          <Route path="/" element={<Navigate to="/books" replace />} />
+        </Routes>
+      </Main>
     </>
   );
 }
